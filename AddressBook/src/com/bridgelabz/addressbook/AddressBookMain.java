0941699bab1cb8d6780b7b6b4735f4df7b41ddc4
SanketTambe39 <sankettambe39@gmail.com>
2021-06-29 09:13:31 +0530

    ADDED: Address Book Edited

--- AddressBook/src/com/bridgelabz/addressbook/AddressBookMain.java
+++ AddressBook/src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -7,18 +7,47 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter How many numbers You want to Add ::");
-		int size = scanner.nextInt();
+	
 		AddressBookItf contact = new AddressBookItf();
 		
-		for (int i = 0; i < size; i++) {
+		while (true) {
+			System.out.println("Please choose from below choices .");
+			System.out.println(" 1 Add Contact "
+					+ "\n 2 Edit Contact "
+					+ "\n 3 Display Contact "
+					+ "\n 4 Quit ");
+			int userChoice = scanner.nextInt();
+			switch (userChoice) 
+			{
+				case 1:
+					System.out.println("Enter How many numbers You want to Add ::");
+					int size = scanner.nextInt();
+					for (int i = 0; i < size; i++) {
+						contact.addContact();
+					}			
+					break;
+				case 2:
+					System.out.println("Enter the Email");
+					String email = scanner.next();
+					boolean updated = contact.editContact(email);	
+					if(updated)
+					{
+						System.out.println("Contact Updated.");
+					}
+					else
+					{
+						System.out.println("Contact not found");
+					}
+					break;
+				case 3:	
+					contact.diaplayContacts();
+					break;
+				default:
+					System.out.println("You just Quit");
+					System.exit(0);
+			}
 			
-			contact.addContact();
-			contact.diaplayContacts();
-						
 		}
-		
 	}
 
 }
