dbdc137530aea2d7f4e9b392b1f91f0145657e90
SanketTambe39 <sankettambe39@gmail.com>
2021-06-29 08:20:19 +0530

    ADDED: Creation of AddressBook

--- AddressBook/src/com/bridgelabz/addressbook/AddressBookMain.java
+++ AddressBook/src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,11 +1,24 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
 		
-
+		System.out.println("Enter How many numbers You want to Add ::");
+		int size = scanner.nextInt();
+		AddressBookItf contact = new AddressBookItf();
+		
+		for (int i = 0; i < size; i++) {
+			
+			contact.addContact();
+			contact.diaplayContacts();
+						
+		}
+		
 	}
 
 }
